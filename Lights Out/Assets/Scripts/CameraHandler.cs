e5f2d63a04896ce8aa564605a94a14610d0bf6f5
Doggitoz <cpwheeler4@gmail.com>
2022-01-28 23:43:54 -0600

    Temporary Camera Handler
    - Makes camera follow character's X value

--- Lights Out/Assets/Scripts/CameraHandler.cs
+++ Lights Out/Assets/Scripts/CameraHandler.cs
@@ -6,9 +6,11 @@ public class CameraHandler : MonoBehaviour
 {
 
     //This wasn't working, it couldn't recognize the GameObject
-    //[SerializeField] private GameObject playableCharacter;
+    [SerializeField] private GameObject playableCharacter;
     [SerializeField] private Camera mainCamera;
     private float cameraX = 0f;
+    private float playerX = 0f;
+    private Vector3 destination;
 
     // Start is called before the first frame update
     void Start()
@@ -19,7 +21,9 @@ public class CameraHandler : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        cameraX = mainCamera.transform.position.x;
-        Debug.Log(cameraX);
+        cameraX = mainCamera.transform.position.x; //Get camera position each frame
+        playerX = playableCharacter.transform.position.x;
+        destination = new Vector3(playerX, 0, -10); //Create a Tween if have the time
+        mainCamera.transform.position = destination;
     }
 }
