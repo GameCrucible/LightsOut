1e85738175a9e67092cd6c3c8cc9f2a6f141b744
Doggitoz <cpwheeler4@gmail.com>
2022-01-28 23:19:26 -0600

    Update CameraHandler.cs

--- Lights Out/Assets/Scripts/CameraHandler.cs
+++ Lights Out/Assets/Scripts/CameraHandler.cs
@@ -5,20 +5,21 @@ using UnityEngine;
 public class CameraHandler : MonoBehaviour
 {
 
-    [SerializeField] private GameObject playableCharacter;
-    private static Camera main;
+    //This wasn't working, it couldn't recognize the GameObject
+    //[SerializeField] private GameObject playableCharacter;
+    [SerializeField] private Camera mainCamera;
     private float cameraX = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        mainCamera = Camera.main;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        cameraX = Camera.Transform.Position.X;
+        cameraX = mainCamera.transform.position.x;
         Debug.Log(cameraX);
     }
 }
