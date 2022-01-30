44535b9b3a5abb9f9be72eced20619d8a2d27460
Doggitoz <cpwheeler4@gmail.com>
2022-01-30 09:26:15 -0600

    Added vertical cam fluidity

--- Lights Out/Assets/Scripts/CameraHandler.cs
+++ Lights Out/Assets/Scripts/CameraHandler.cs
@@ -8,13 +8,10 @@ public class CameraHandler : MonoBehaviour
     [SerializeField] private GameObject playableCharacter;
     [SerializeField] private Camera mainCamera;
     [SerializeField] private float cameraMoveSpeed;
-    /*private float cameraX = 0f;
-    private float cameraY = 0f;*/
-    private Vector3 playerPos;
-    private Vector3 cameraPos;
-    /*private Vector3 camDestination;
-    private float z = -10;
-    private Vector3 destination = new Vector3(0, 0, -10);*/
+    private Vector2 playerPos;
+    private Vector2 cameraPos;
+    float yPos = 0;
+    float xPos = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -25,35 +22,23 @@ public class CameraHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        cameraPos = mainCamera.transform.position;
         playerPos = playableCharacter.transform.position;
-        mainCamera.transform.position = new Vector3(playerPos.x, 0, -10);
 
-
-
-        /* ---------Fix later if have time, for tweening camera and more fluid camera movement
-        cameraX = mainCamera.transform.position.x; //Get camera position each frame
-        cameraY = mainCamera.transform.position.y;
-        cameraPos = mainCamera.transform.position;
-        playerPos = playableCharacter.transform.position;
-        
-
-        //Logic that handles detecting new camera position
-        if (Mathf.Abs(playerPos.x - cameraX) >= 1.5f) 
-        {
-            Debug.Log(Mathf.Abs(playerPos.x - cameraX));
-            destination = new Vector3.Lerp(cameraPos, 0, -10); //Create a Tween if have the time
-            //Add nested if to handle y changes, else just move to new X
+        if (playerPos.y > 4) {
+            yPos = playerPos.y;
+        } else if (playerPos.y > 2) {
+            yPos = playerPos.y - (4 - playerPos.y); //Range of playerPos.y - 2 to playerPos.y over [2, 4]
+        } else {
+            yPos = 0;
         }
-        camDestination = new Vector3(playerPos.x, 0, -10);
 
 
-        mainCamera.transform.position = Vector3.Lerp(cameraPos, camDestination, cameraMoveSpeed);
+        if (playerPos.x >= -1) {
+            xPos = playerPos.x;
+        } else {
+            xPos = -1;
+        }
 
-
-
-
-        /*destination = new Vector3(playerPos.x, 0, -10); //Create a Tween if have the time
-        mainCamera.transform.position = destination;*/
+        mainCamera.transform.position = new Vector3(xPos, yPos, -10);
     }
 }
